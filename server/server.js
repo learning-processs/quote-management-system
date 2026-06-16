e6da4ffe7ef7ddd56d7971c53a2a84e84756257b
Anu025 <anu001125@gmail.com>
2026-06-16 21:17:00 +0530

    fix: explicit CORS handling and options preflight routing for vercel serverless deployment

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ const corsOptions = {
 
 // Middleware
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+// app.options('/*', cors(corsOptions));
 app.use(express.json());
 
 app.use('/api/auth',authRouter);
